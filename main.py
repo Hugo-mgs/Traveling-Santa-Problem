9e1917902f602156293fc4ba0c0d449ab0759a6a
Daniel Marques <up202306365@up.pt>
2026-04-09 12:29:33 +0100

    Created score function and modified main

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import pandas as pd
 from core.graph import Graph, make_edge, Edge
 
 def main():
-    in_file = pd.read_csv("input/random_50.csv")
-    out_file = pd.read_csv("solution.csv")
-    nearest_neighbour(in_file)
+    graph = Graph.from_csv("input/random_50.csv")
+    path1, path2 = nearest_neighbour(graph)
+    print(score(graph, path1, path2))
 
 def path_distance(graph: Graph, path: list[int]) -> float:
     dist = sum(graph.distance(path[i], path[i+1]) for i in range(len(path) - 1))
     return round(dist, 2)
+
+def score(graph: Graph, path1: list[int], path2: list[int]) -> float:
+    return max(path_distance(graph, path1), path_distance(graph, path2))
 
 def nearest_neighbour(graph: Graph) -> tuple[list[int], list[int]]:
     used_edges: set[Edge] = set()
@@ -25,7 +28,8 @@ def nearest_neighbour(graph: Graph) -> tuple[list[int], list[int]]:
             if not unvisited:
                 continue
             current_city = path[-1]
-            next_city = graph.nearest_neighbours(current_city, exclude_edges=used_edges)[0]
+            candidates = graph.nearest_neighbours(current_city, exclude_edges=used_edges)
+            next_city = next(c for c in candidates if c in unvisited)
             path.append(next_city)
             used_edges.add(make_edge(current_city, next_city))
             unvisited.remove(next_city)
