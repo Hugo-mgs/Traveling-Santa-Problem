9c0f17e27d32f04eb4a41e05aad765e66dc04a77
Daniel Marques <up202306365@up.pt>
2026-04-09 12:15:41 +0100

    Implemented nearest neighbor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from core.graph import Graph, Edge, City
+from core.graph import Graph, make_edge, Edge
 
 def main():
     in_file = pd.read_csv("input/random_50.csv")
@@ -14,26 +14,29 @@ def total_distance(path):
     
     return round(dist, 2)
 
-def nearest_neighbour(df):
-    # gets all the node ids ignoring the first
-    ids = df.index.values[1:]
-    # gets and pairs all x and y values ignoring the first
-    xy = np.array([df.x.values, df.y.values]).T[1:]
-    path = [0,]
-    for _ in range(len(df)-1):
-        # gets x and y from last id in path
-        last_x, last_y = df.x[path[-1]], df.y[path[-1]]
-        # calculates the distance between every node and the last one
-        dist = ((xy - np.array([last_x,last_y]))**2).sum(-1)
-        # gets the nearest one
-        nearest_idx = dist.argmin()
-        path.append(ids[nearest_idx])
-        # removes the found node from ids and xy
-        ids = np.delete(ids, nearest_idx, axis=0)
-        xy  = np.delete(xy,  nearest_idx, axis=0)
-    # adds the origin to the end
-    path.append(0)
-    return path
+def nearest_neighbour(graph: Graph) -> tuple[list[int], list[int]]:
+    used_edges: set[Edge] = set()
+
+    path1 = [0]
+    path2 = [0]
+
+    unvisited1 = set(graph.city_ids) - {0}
+    unvisited2 = set(graph.city_ids) - {0}
+
+    while unvisited1 or unvisited2:
+        for path, unvisited in [(path1, unvisited1), (path2, unvisited2)]:
+            if not unvisited:
+                continue
+            current_city = path[-1]
+            next_city = graph.nearest_neighbours(current_city, exclude_edges=used_edges)[0]
+            path.append(next_city)
+            used_edges.add(make_edge(current_city, next_city))
+            unvisited.remove(next_city)
+    
+    path1.append(0)
+    path2.append(0)
+
+    return path1, path2
 
 if __name__ == "__main__":
     main()
