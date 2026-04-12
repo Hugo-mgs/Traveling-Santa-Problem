876e525c45c435ffd46fb33bb2a1912f12c52a5b
Hugo <up202307383@up.pt>
2026-04-12 16:30:06 +0100

    Add time measurement

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from algorithms.simulated_annealing import simulated_annealing
 from algorithms.genetic_algorithm import genetic_algorithm
 from algorithms.hill_climbing import hill_climbing
 
+from timeit import default_timer as timer
 import os
 
 def main():
-    file_name = input("Input file name (default random_50): ")
-    file_name = "input/random_50.csv" if file_name == "" else "input/" + file_name + ".csv"
+    file_name = input("Input file name (default santa_100): ")
+    file_name = "input/santa_100.csv" if file_name == "" else "input/" + file_name + ".csv"
     if not os.path.exists(file_name):
         print("File does not exist")
         return
@@ -24,6 +25,8 @@ def main():
     print("[5] Genetic Algorithm")
     print("[6] Hill Climbing")
     alg = input("Choose an algorithm: ")
+
+    start = timer()
     
     if not alg == "1":
         # Precompute candidate lists for 2-opt (critical for performance on larger instances)
@@ -56,10 +59,13 @@ def main():
     else:
         print("Invalid algorithm")
         return
+    
+    end = timer()
 
     dist1, dist2 = sol.distances(graph)
     print(f"Path1 distance: {dist1}, Path2 distance: {dist2}")
     print(f"Score: {sol.score(graph)}")
+    print(f"Time elapsed: {round(end - start, 2)}s")
     output_filename = "solution.csv"
     sol.to_csv(output_filename)
     print(f"Solution saved to {output_filename}")
